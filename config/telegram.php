702da07534c049715037ef03a6587f0250db2548
Socheat <cheatgaming1111@gmail.com>
2026-07-31 08:43:36 +0700

    security: remove hardcoded telegram bot token from config/telegram.php

--- config/telegram.php
+++ config/telegram.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../core/classes/CookieCrypt.php';
 $isLocal = mc_is_local_request();
 
 return [
-    'bot_token'      => CookieCrypt::decrypt(mc_env('MC_TELEGRAM_BOT_TOKEN', '8688625817:AAFWjjODBE05brt-iQWpHEXvPJv7hTg2KdY')),
+    'bot_token'      => CookieCrypt::decrypt(mc_env('MC_TELEGRAM_BOT_TOKEN', '')),
     'chat_id'        => mc_env('MC_TELEGRAM_CHAT_ID', ''),
     'webhook_secret' => mc_env('MC_TELEGRAM_WEBHOOK_SECRET', 'mcu_tele_sec_99'),
     // Local = empty (use polling/sync_telegram.php), Live = webhook URL
